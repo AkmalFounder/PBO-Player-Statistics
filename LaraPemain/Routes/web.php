695d91e0ac6ee4b3e614b9e68e476b41b5fb61bd
akmalikhsan <63221748+AkmalFounder@users.noreply.github.com>
2022-01-22 23:45:01 +0700

    Update web.php

--- LaraPemain/Routes/web.php
+++ LaraPemain/Routes/web.php
@@ -1,7 +1,7 @@
  <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PemainController;
+use App\Http\Controllers\FooballController;
 use App\Http\Controllers\statistikController;
 
 /*
@@ -23,9 +23,17 @@ Route::get('/home', function (){
     return view('layouts.Dashboard');
 });
 
-Route::get('/players', function (){
-    return view('statistik.pemain');
-});
+Route::get('/players', [FooballController::class, 'index'])->name('players');
+
+Route::get('/ubahdata/{id}', [FooballController::class, 'ubahdata'])->name('ubahdata');
+
+Route::post('/updatedata/{id}', [FooballController::class, 'updatedata'])->name('updatedata');
+
+//Route::get('/players', ['as' => 'statistik.pemain', 'players' => 'PemainController@pemain']);
+
+// Route::get('/players', function (){
+//     return view('statistik.pemain');
+// });
 
 Route::get('/teams', function (){
     return view('statistik.index');
